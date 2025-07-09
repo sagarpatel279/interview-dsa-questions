b9a636356939bd9547a5d461b0c9a7d3ae26a817
Sagar <patelsagar913@gmail.com>
2025-07-09 19:50:37 +0530

    corrected the file name

--- KnightToTargetMinSteps.java
+++ KnightToTargetMinSteps.java
@@ -1,37 +1,56 @@
+import java.util.*;
 
 public class KnightToTargetMinSteps {
     private static int sum;
     public static void main(String[] args) {
-        // TreeNode t7=new TreeNode(7,null,null);
-        TreeNode t6=new TreeNode(6,null,null);
-        TreeNode t5=new TreeNode(5,null,null);
-        TreeNode t4=new TreeNode(4,null,null);
-        TreeNode t3=new TreeNode(3,t6,null);
-        TreeNode t2=new TreeNode(2,t4,t5);
-        TreeNode t1=new TreeNode(1,t2,t3);
-        treeSum(t1,0);
-        System.out.println(sum);
+        int[][] board=new int[8][8];
+        int[] k={5,2};
+        int[] t={7,1};
+        System.out.println(getMinSteps(board,k,t));
     }
     
-    private static void treeSum(TreeNode root,int num){
-        if(root==null){return;}
-        num=num*10+root.val;
-        if(root.left==null && root.right==null){
-            sum+=num;
+    private static int getMinSteps(int[][] board,int[] k,int[] t){
+        int n=board.length, m=board[0].length;
+        int minSteps=Integer.MAX_VALUE;
+        Queue<int[]> queue=new LinkedList<>();
+        int[][] dir={{2,1},{-2,1},{2,-1},{-2,-1},
+                    {1,2},{-1,2},{1,-2},{-1,-2}};
+        boolean[][] visit=new boolean[n][m];
+        int[][] dist=new int[n][m];
+        queue.add(k);
+        while(!queue.isEmpty()){
+            int[] currK=queue.poll();
+            int k_i=currK[0], k_j=currK[1];
+            visit[k_i][k_j]=true;
+            for(int d=0;d<dir.length;d++){
+                int i= k_i+ dir[d][0];
+                int j= k_j+ dir[d][1];
+                if(isValidMove(i,j,n,m) && !visit[i][j]){
+                    queue.add(new int[]{i,j});
+                    dist[i][j]=dist[k_i][k_j]+1;
+                    if(t[0]==i && t[1]==j){
+                        minSteps=Math.min(minSteps,dist[i][j]);
+                    }
+                }
+            }
         }
-        treeSum(root.left,num);
-        treeSum(root.right,num);
-        num/=10;
+        return minSteps;
+    }
+    
+    private static boolean isValidMove(int i,int j,int n,int m){
+        return i>=0 && j>=0 && i<n && j<m;
     }
 }
 
-class TreeNode{
-    int val;
-    TreeNode left;
-    TreeNode right;
-    public TreeNode(int val, TreeNode left, TreeNode right){
-        this.val=val;
-        this.left=left;
-        this.right=right;
-    }
-}
+
+//   0 1 2 3 4 5 6 7
+// 0 0 0 0 0 0 0 0 0
+// 1 0 0 0 0 0 0 0 0    
+// 2 0 0 0 0 0 0 0 0
+// 3 0 0 0 0 0 0 0 0
+// 4 0 0 0 0 0 0 0 0
+// 5 0 0 K 0 0 0 0 0
+// 6 0 0 0 0 0 0 0 0
+// 7 0 T 0 0 0 0 0 0
+    
+    
